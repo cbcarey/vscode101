2c420c97db2e5e24cfd81ac35dceea54a95b55a9
cbcarey <cbcarey1@gmail.com>
2025-04-14 14:23:04 -0500

    Minor edits

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 from datetime import date
 
+
+
 def say_hello(name):
+    name = "Craig"
     print("Hello, " + name)
 
 say_hello("VS Code")
@@ -9,3 +12,4 @@ def say_day_of_the_week(date):
     print("Today is " + date.strftime("%A"))
 
 say_day_of_the_week(date.today())
+
