6647dc251165c834ea5df01b8628e625260dd52c
cbcarey <cbcarey1@gmail.com>
2025-04-13 21:46:21 -0500

    Changes made with Tutorial Videos

---
+++ hello.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+def say_hello(name):
+    print("Hello, " + name)
+
+say_hello("VS Code")
+
+def say_day_of_the_week(date):
+    print("Today is " + date.strftime("%A"))
+
+say_day_of_the_week(date.today())
